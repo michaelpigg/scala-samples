10736d7217239dbe571d3cda65d2b367fabd917b
Michael Pigg <mikepigg@highlylogical.com>
2015-01-19 11:41:44 -0500

    Added tests

--- src/test/scala/FutureSpec.scala
+++ src/test/scala/FutureSpec.scala
@@ -1,9 +1,26 @@
 import org.scalatest.{ WordSpec, Matchers}
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class FuturesSpec extends WordSpec with Matchers {
-  "Futures" should {
+  "Await.result" should {
+
+    def delayingFuture(value: String): Future[String] = future {
+      Thread.sleep(5000)
+      value
+    }
     "allow blocking, even if it's not a good idea" in {
-      fail("not implemented")
+      println("before Await")
+      val result = Await.result(delayingFuture("badbusiness"), 10 seconds)
+      println(s"result $result")
+      println("after Await")
+      result shouldBe "badbusiness"
+    }
+    "throw an exception if future does not return within timeout" in {
+      intercept[TimeoutException] {
+        Await.result(delayingFuture("toolong"), 1 seconds)
+      }
     }
   }
 }
