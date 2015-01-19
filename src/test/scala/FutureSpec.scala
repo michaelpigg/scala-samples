577951d00978e323a5107cc7fb5d95a2291e8f33
Michael Pigg <mikepigg@highlylogical.com>
2015-01-19 10:44:07 -0500

    Update FutureSpec.scala

--- src/test/scala/FutureSpec.scala
+++ src/test/scala/FutureSpec.scala
@@ -2,7 +2,7 @@ import org.scalatest.{ WordSpec, Matchers}
 
 class FuturesSpec extends WordSpec with Matchers {
   "Futures" should {
-    "allow blocking, even if it's not a good idea" {
+    "allow blocking, even if it's not a good idea" in {
       fail("not implemented")
     }
   }
