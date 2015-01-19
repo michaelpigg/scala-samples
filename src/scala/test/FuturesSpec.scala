a5a4b39b84171d5b5675acff101eb608a12582a5
Michael Pigg <mikepigg@highlylogical.com>
2015-01-19 10:39:01 -0500

    Delete FuturesSpec.scala

--- src/scala/test/FuturesSpec.scala
+++
@@ -1,9 +0,0 @@
-import org.scalatest.{ WordSpec, Matchers}
-
-class FuturesSpec extends WordSpec with Matchers {
-  "Futures" should {
-    "allow blocking, even if it's not a good idea" {
-      fail("not implemented")
-    }
-  }
-}
